747710f7ebdc278ddd27a829ca437254f0eb0c25
aroranipun12 <aroranipun1@gmail.com>
2021-11-24 14:15:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ app: 'Natours', message: 'Hello from the server side!' });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app running on port ${port} ...`);
+});
