1a682d3b15a8cba72b1a134a3f759ae5c770715a
aroranipun12 <aroranipun1@gmail.com>
2021-12-17 16:07:53 +0530

    implemented pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -73,6 +73,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
       // - means excluding
     }
+
+    // 4. Pagination
+    //default is page 1 , *1 for string to number
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // execute query
     const tours = await query;
 
@@ -94,7 +107,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err.message,
     });
   }
 };
