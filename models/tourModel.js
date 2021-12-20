44b38090a8e3077adfd1f1734bd695d125e08e1d
aroranipun12 <aroranipun1@gmail.com>
2021-12-20 15:05:40 +0530

    aggregation pipeline routes

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,57 +1,66 @@
 const mongoose = require('mongoose');
 
 // specifying a schema for tours
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: { type: Number, default: 4.5 },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a type'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trime: true, // remove white space from beginning and end
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String, // storing a reference to the image on fs
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false, //to exclude it from client(response m nahi jayega ye field)
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: { type: Number, default: 4.5 },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a type'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    required: [true, 'A tour must have a summary'],
-    trime: true, // remove white space from beginning and end
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String, // storing a reference to the image on fs
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false, //to exclude it from client(response m nahi jayega ye field)
-  },
-  startDates: [Date],
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
 // model
 const Tour = mongoose.model('Tour', tourSchema);
 
